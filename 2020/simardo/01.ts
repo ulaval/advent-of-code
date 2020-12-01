1f7f8028e7099d2c20f5abbb9cf3909afc43e2a4
simardo <simardo@gmail.com>
2020-12-01 06:22:28 -0500

    part 1 - simardo

---
+++ 2020/simardo/01.ts
@@ -0,0 +1,36 @@
+// https://github.com/simardo/adventofcode2020/blob/master/src/01/01.ts
+
+console.log('1er décembre');
+
+// part 1
+function doPart1(input: string): void {
+    const nums: number[] = input.split('\n').map(s => Number.parseInt(s));
+
+    const result: number = nums.reduce((p, n) => {
+        const match: number | undefined = nums.find(nn => nn + n === 2020);
+        return match ? n * match : p;
+    }, 0);
+
+    console.log(result);
+}
+
+// doPart1(INPUT);
+
+// part 2
+function doPart2(input: string): void {
+    const map: Set<number> = new Set();
+    const nums: number[] = input.split('\n').map(s => {
+        const n: number = Number.parseInt(s)
+        map.add(n);
+        return n;
+    });
+
+    const result: number = nums.reduce((p, n) => {
+        const match: number | undefined = nums.find(nn => map.has(2020 - n - nn) ? nn : undefined);
+        return match ? n * match * (2020 - n - match) : p;
+    }, 0);
+
+    console.log(result);
+}
+
+// doPart2(INPUT);
