fcea9e2432a17d96170aa994d1ad13b06c52cc10
Vincent Guillemette <vincentguillemette1981@gmail.com>
2020-12-03 00:30:38 -0500

    star 1

--- 2020/vincent guillemette  (vigui13)/03/star-1/1.js
+++ 2020/vincent guillemette  (vigui13)/03/star-1/1.js
@@ -1,3 +1,27 @@
-let solution;
+const map = require('./1-input');
 
-console.log(solution);
+const slop = {
+    x: 3,
+    y: -1,
+};
+const pos = {
+    x: 0,
+    y: 0,
+};
+
+console.log(
+    map.reduce((acc) => {
+        pos.x = (pos.x + slop.x) % map[0].length;
+        pos.y += slop.y * -1;
+
+        if (pos.y >= map.length) {
+            return acc;
+        }
+
+        if (map[pos.y][pos.x] === '#') {
+            acc++;
+        }
+
+        return acc;
+    }, 0)
+);
