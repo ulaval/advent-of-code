56520ea6f7411251f69c767d6fb82d251b5e7941
Manuel Grenier <manuel.grenier@dti.ulaval.ca>
2020-12-18 07:31:24 -0500

    The power of regex

---
+++ 2020/Manuel Grenier (kh0brak)/D18/day18b.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+
+#
+use strict;
+use warnings;
+use Data::Dumper;
+use Storable qw(dclone);
+
+open(FILE, 'input.txt') or die;
+
+my $sum = 0;
+while (<FILE>) {
+	chomp;
+	# =~ s/\s+//g
+	$sum += evaluate($_ );
+}
+print $sum;
+
+sub evaluate{
+	my $equation = shift;
+	
+	return $equation if $equation =~ /^\d+$/;
+	
+	while (my ($oper) = $equation =~ /(\([^()]+\))/ ){
+		$oper =~ s/[\(\)]//g;
+		my $res = evaluate( $oper );
+		$equation =~ s/(\([^()]+\))/$res/;
+	}
+
+	while (my ($oper) = $equation =~ /(\d+ \+ \d+)/){
+		my $res = eval($oper);
+		$equation =~ s/(\d+ \+ \d+)/$res/;	
+	}
+	
+	while (my ($oper) = $equation =~ /(\d+ \* \d+)/){
+		my $res = eval($oper);
+		$equation =~ s/(\d+ \* \d+)/$res/;	
+	}
+	
+	return $equation;
+	
+}
+
+sub removeParenthese{
+	my $equation = shift;
+	
+	while (my ($oper) = $equation =~ /\((\d+)\)/){
+		$equation =~ s/(\(\d+\))/$oper/;	
+	}
+	return $equation;
+}
+
+
+
+
+
