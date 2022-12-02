1ac32efb7df1d4f141f9e727764a28c1b5a7227a
Pierre-Luc Drolet <pierre-luc.drolet@dti.ulaval.ca>
2022-12-02 08:20:04 -0500

    o r i e n t é o b j e t

--- 2022/plucdrol/adventofcode/src/main/java/com/plucdrol/adventofcode/days/Day02.java
+++ 2022/plucdrol/adventofcode/src/main/java/com/plucdrol/adventofcode/days/Day02.java
@@ -1,14 +1,96 @@
 package com.plucdrol.adventofcode.days;
 
+import java.util.Arrays;
+
 import lombok.Getter;
 
 @Getter
 public class Day02 {
 
-    private int reponse1;
+    private final int reponse1;
+    private final int reponse2;
 
     public Day02(String input) {
+        var parties = input.split("\n");
 
+        reponse1 = Arrays.stream(parties).mapToInt(this::getScore1).sum();
+        reponse2 = Arrays.stream(parties).mapToInt(this::getScore2).sum();
+    }
+
+    private int getScore1(String partie) {
+        var partieSplit = partie.split(" ");
+        var choixOpponent = getChoix(partieSplit[0]);
+        var monChoix = getChoix(partieSplit[1]);
+
+        return scorePartie(choixOpponent, monChoix) + valeurChoix(monChoix);
+    }
+
+    private String getChoix(String code) {
+        return switch (code) {
+            case "A", "X" ->  "✊";
+            case "B", "Y" ->  "🤚";
+            case "C", "Z" ->  "✌";
+            default -> throw new IllegalStateException("Unexpected value: " + code);
+        };
+    }
+
+    private int scorePartie(String opponent, String me) {
+        Integer resultat = switch (opponent) {
+            case "✊" -> me.equals("🤚") ? 6 : null;
+            case "🤚" -> me.equals("✌") ? 6 : null;
+            case "✌" -> me.equals("✊") ? 6 : null;
+            default -> throw new IllegalStateException("Unexpected value: " + opponent);
+        };
+        if (resultat == null) {
+            resultat = opponent.equals(me) ? 3 : 0;
+        }
+        return resultat;
+    }
+
+    private int getScore2(String partie) {
+        var partieSplit = partie.split(" ");
+        var choixOpponent = getChoix(partieSplit[0]);
+        var monChoix = getMonChoix(choixOpponent, partieSplit[1]);
+
+        return scorePartie(choixOpponent, monChoix) + valeurChoix(monChoix);
+    }
+
+    private String getMonChoix(String choixOpponent, String code) {
+        return switch (choixOpponent) {
+            case "✊" -> switch (code) {
+                case "X" -> "✌";
+                case "Y" -> "✊";
+                case "Z" -> "🤚";
+                default -> throw new IllegalStateException("Unexpected value: " + code);
+            };
+            case "🤚" -> switch (code) {
+                case "X" -> "✊";
+                case "Y" -> "🤚";
+                case "Z" -> "✌";
+                default -> throw new IllegalStateException("Unexpected value: " + code);
+            };
+            case "✌" -> switch (code) {
+                case "X" -> "🤚";
+                case "Y" -> "✌";
+                case "Z" -> "✊";
+                default -> throw new IllegalStateException("Unexpected value: " + code);
+            };
+            default -> throw new IllegalStateException("Unexpected value: " + choixOpponent);
+        };
+    }
+
+    private int valeurChoix(String choix) {
+        return switch (choix) {
+            case "✊" -> 1;
+            case "🤚" -> 2;
+            case "✌" -> 3;
+            default -> throw new IllegalStateException("Unexpected value: " + choix);
+        };
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Reponse 1: '%s'; Reponse 2: '%s'", reponse1, reponse2);
     }
 
 }
--- 2022/plucdrol/adventofcode/src/test/java/com/plucdrol/adventofcode/days/Day02Test.java
+++ 2022/plucdrol/adventofcode/src/test/java/com/plucdrol/adventofcode/days/Day02Test.java
@@ -7,40 +7,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class Day02Test {
 
     private static final String INPUT_TEST = """
-            C Y
-            C Z
-            B Z
-            A Z
-            A Z
-            A Y
-            A Z
-            C Y
-            C Z
-            A Y
             A Y
             B X
-            A Y
             C Z
-            C Z
-            B X
-            C Z
-            A Z
-            B Y
-            C Z
-            A Y
-            C X
-            B Y
-            A Z
-            B Y
-            C Z
-            B Z
             """;
 
     @Test
     void doTheBartman() {
         var day02 = new Day02(INPUT_TEST);
 
-        assertThat(day02.getReponse1()).isPositive();
+        assertThat(day02.getReponse1()).isEqualTo(15);
+        assertThat(day02.getReponse2()).isEqualTo(12);
     }
 
 }
