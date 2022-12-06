8c581fc9d9731111ce88f6501f0c651d805d8b69
Manuel Grenier <manuel.grenier@dti.ulaval.ca>
2022-12-06 09:24:20 -0500

    1 ligne de code c'est mieux

--- 2022/Manuel Grenier (kh0brak)/D02/day2a.pl
+++ 2022/Manuel Grenier (kh0brak)/D02/day2a.pl
@@ -8,7 +8,6 @@ use File::Slurp;
 use List::Util qw/max min sum/;
 
 
-my %key = (	'A X' => 4, 'A Y' => 8, 'A Z' => 3, 'B X' => 1, 'B Y' => 5, 'B Z' => 9, 'C X' => 7, 'C Y' => 2, 'C Z' => 6);
-print sum map { $key{$_} } split /\n/, read_file('input.txt') ;
+print sum map { {'A X' => 4, 'A Y' => 8, 'A Z' => 3, 'B X' => 1, 'B Y' => 5, 'B Z' => 9, 'C X' => 7, 'C Y' => 2, 'C Z' => 6}->{$_} } split /\n/, read_file('input.txt') ;
 
 
--- 2022/Manuel Grenier (kh0brak)/D02/day2b.pl
+++ 2022/Manuel Grenier (kh0brak)/D02/day2b.pl
@@ -7,6 +7,4 @@ use Data::Dumper;
 use File::Slurp;
 use List::Util qw/max min sum/;
 
-
-my %key = (	'A X' => 3, 'A Y' => 4, 'A Z' => 8, 'B X' => 1, 'B Y' => 5, 'B Z' => 9, 'C X' => 2, 'C Y' => 6, 'C Z' => 7 );
-print sum map { $key{$_} } split /\n/, read_file('input.txt') ;
+print sum map { {'A X' => 3, 'A Y' => 4, 'A Z' => 8, 'B X' => 1, 'B Y' => 5, 'B Z' => 9, 'C X' => 2, 'C Y' => 6, 'C Z' => 7}->{$_} } split /\n/, read_file('input.txt') ;
